cbccc11d5e41ec2682b8f1d8f2f605bdc62bb8c8
hungconth <76127579+hungconth@users.noreply.github.com>
2023-05-12 00:10:57 +0700

    update customer

--- api_customer.py
+++ api_customer.py
@@ -9,13 +9,15 @@ import pyodbc  # pip install pyodbc
 templates = Jinja2Templates(directory="templates")
 app = FastAPI()
 
+
+
 def connect():
     conn = pyodbc.connect('Driver={SQL Server};'
-                        'Server=TLE;'
-                        'Database=shopee;'
+                        'Server=TLE\CSDLPTN1;'
+                        'Database=manh2_shopee;'
                         'Trusted_Connection=yes;')
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM [shopee].[dbo].[customer]')
+    cursor.execute('SELECT * FROM [customer]')
     rows = cursor.fetchall()
     for row in rows:
         print(row)
@@ -24,14 +26,14 @@ def connect():
 
 @app.get("/")
 async def home(request: Request, cursor: pyodbc.Cursor = Depends(connect)):
-    cursor.execute('SELECT * FROM [shopee].[dbo].[customer]')
+    cursor.execute('SELECT * FROM [customer]')
     rows = cursor.fetchall()
     return templates.TemplateResponse("index.html", {"request": request, "customers": rows})
 
 @app.post("/add")
-async def add(request: Request, customer_name: str = Form(...), phone_number: str = Form(...), address: str = Form(...), cursor: pyodbc.Cursor = Depends(connect)):
+async def add(request: Request, address: str = Form(...),budget: str = Form(...), cursor: pyodbc.Cursor = Depends(connect)):
     # print(f"insert into [customer] (name, id_headquarter, phone_number, address) values  ('{customer_name}', '{id_headquarter}', '{phone_number}', '{address}');")
-    cursor.execute(f"insert into [customer] (name, id_headquarter, phone_number, address) values  ('{customer_name}', '{1}', '{phone_number}', '{address}');")
+    cursor.execute(f"insert into [customer] (id_headquarter, address, budget) values  ( '{1}', '{address}', '{budget}');")
     cursor.commit()
     return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
 
@@ -41,23 +43,23 @@ async def addnew(request: Request):
 
 @app.get("/edit/{customer_id}")
 async def edit(request: Request, customer_id: int, cursor: pyodbc.Cursor = Depends(connect)):
-    # print(f"SELECT * FROM [shopee].[dbo].[customer] WHERE id = {customer_id}")
-    cursor.execute(f"SELECT * FROM [shopee].[dbo].[customer] WHERE id = {customer_id}")
+    # print(f"SELECT * FROM [customer] WHERE id = {customer_id}")
+    cursor.execute(f"SELECT * FROM [customer] WHERE id = {customer_id}")
     customer = cursor.fetchall()
     print(customer[0])
     return templates.TemplateResponse("edit.html", {"request": request, "customer": customer[0]})
 
 @app.post("/update/{customer_id}")
-async def update(request: Request, customer_id: int, customer_name: str = Form(...), id_headquarter: str = Form(...), phone_number: str = Form(...), address: str = Form(...), cursor: pyodbc.Cursor = Depends(connect)):
-    # print(f"UPDATE [shopee].[dbo].[customer] SET name = '{customer_name}', id_headquarter = '{id_headquarter}', phone_number = '{phone_number}', address = '{address}' WHERE id = {customer_id}")
-    cursor.execute(f"UPDATE [shopee].[dbo].[customer] SET name = '{customer_name}', id_headquarter = '{id_headquarter}', phone_number = '{phone_number}', address = '{address}' WHERE id = {customer_id}")
+async def update(request: Request, customer_id: int, address: str = Form(...),budget: str = Form(...),  cursor: pyodbc.Cursor = Depends(connect)):
+    # print(f"UPDATE [customer] SET name = '{customer_name}', id_headquarter = '{id_headquarter}', phone_number = '{phone_number}', address = '{address}' WHERE id = {customer_id}")
+    cursor.execute(f"UPDATE [customer] SET  id_headquarter = '{1}', address = '{address}', budget = '{budget}' WHERE id = {customer_id}")
     cursor.commit()
     return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
 
 @app.get("/delete/{user_id}")
 async def delete(request: Request, user_id: int, cursor: pyodbc.Cursor = Depends(connect)):
-    # print(f"DELETE FROM [shopee].[dbo].[customer] WHERE id = {user_id}")
-    cursor.execute(f"DELETE FROM [shopee].[dbo].[customer] WHERE id = {user_id}")
+    # print(f"DELETE FROM [customer] WHERE id = {user_id}")
+    cursor.execute(f"DELETE FROM [customer] WHERE id = {user_id}")
     cursor.commit()
     return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
 
@@ -69,7 +71,7 @@ async def get_customers():
                         'Database=shopee;'
                         'Trusted_Connection=yes;')
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM [shopee].[dbo].[customer]')
+    cursor.execute('SELECT * FROM [customer]')
     rows = cursor.fetchall()
     customers = []
     for row in rows:
